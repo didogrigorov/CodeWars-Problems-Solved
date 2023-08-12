b041a4fdeb9bb7ebf96ade8c5f50b95df2325f6b
didogrigorov <dido.grigorov@gmail.com>
2023-08-12 21:51:22 +0300

    5 problems solved.

---
+++ Welcome!.py
@@ -0,0 +1,26 @@
+def greet(language):
+    languages = {
+        'english': 'Welcome',
+        'czech': 'Vitejte',
+        'danish': 'Velkomst',
+        'dutch': 'Welkom',
+        'estonian': 'Tere tulemast',
+        'finnish': 'Tervetuloa',
+        'flemish': 'Welgekomen',
+        'french': 'Bienvenue',
+        'german': 'Willkommen',
+        'irish': 'Failte',
+        'italian': 'Benvenuto',
+        'latvian': 'Gaidits',
+        'lithuanian': 'Laukiamas',
+        'polish': 'Witamy',
+        'spanish': 'Bienvenido',
+        'swedish': 'Valkommen',
+        'welsh': 'Croeso'
+    }
+    if language in languages:
+        return languages[language]
+    else:
+        return "Welcome"
+
+print(greet(2))
